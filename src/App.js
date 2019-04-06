f4bf3ad9a1f3162d966fe14344d5ab0e78d56f9c
BenSchuddeboom <Benzel@live.nl>
2019-04-06 10:22:58 +0200

    Step 2 complete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const data = {
+	"Ivel Z3": {
+		manufacturer: "Ivasim",
+		year: 1969,
+		origin: "Croatia"
+	},
+	"Bally Astrocade": {
+		manufacturer: "Bally Consumer Products",
+		year: 1977,
+		origin: "USA"
+	},
+	"Sord M200 Smart Home Computer": {
+		manufacturer: "Sord Computer Corporation",
+		year: 1971,
+		origin: "Japan"
+	},
+	"Commodore 64": {
+		manufacturer: "Commodore",
+		year: 1982,
+		origin: "USA"
+	}
+}
+
 class App extends Component {
+  state = {
+
+  }
+
+  updateSelection = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  renderOptions = (data) => {
+  	const models = Object.keys(data)
+  	return models.map(model => {
+  		return (
+  			<option value={model} key={model}>
+  				{model} ({data[model].year})
+  			</option>
+  		)
+  	})
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  	return (
+  		<div className="App">
+  			<select onChange={this.updateSelection} value={this.state.value}>
+  				<option value="">-- Pick a model --</option>
+  				{this.renderOptions(data)}
+  			</select>
+  		</div>
+  	);
   }
 }
 
